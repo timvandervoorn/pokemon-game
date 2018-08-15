5abeaabc7dbb2297ade6ddbebe32187fa66a67d2
timvandervoorn <tvanderv@gmail.com>
2018-08-15 10:24:21 +0200

    bla

--- pokemon-client/src/constants.js
+++ pokemon-client/src/constants.js
@@ -1,3 +1,8 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = "http://localhost:4000"
 
-export const localStorageJwtKey = 'currentUserJwt'
+export const localStorageJwtKey = "currentUserJwt"
+
+export const attack = "attack"
+export const item = "item"
+export const pokemon = "pokemon"
+export const run = "run"
