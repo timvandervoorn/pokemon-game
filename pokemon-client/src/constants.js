ea5bd45d045f72af99f9376c68b508285c07e67b
timvandervoorn <tvanderv@gmail.com>
2018-08-17 14:08:17 +0200

    Added item in database

--- pokemon-client/src/constants.js
+++ pokemon-client/src/constants.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://localhost:4000"
-// export const baseUrl = "http://172.16.30.157:4000"
+// export const baseUrl = "http://localhost:4000"
+export const baseUrl = "http://172.16.30.157:4000"
 
 export const localStorageJwtKey = "currentUserJwt"
 
